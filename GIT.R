d280e9cd233e217372dd434c0bf841be2605a139
Defreitasgb <gessicafreitas.mm@gmail.com>
2021-11-09 22:05:43 -0300

    Teste

--- GIT.R
+++ GIT.R
@@ -1,16 +1,19 @@
 library(usethis)
 library(gitcreds)
 
+#uma vez na vida
 use_git_config(
   user.name = "Defreitasgb",
   user.email = "gessicafreitas.mm@gmail.com"
 )
+#uma vez por projeto
 use_git()
 
-use_github()
 
+#uma vez na vida
 create_github_token()
 
 gitcreds::gitcreds_set()
 
-?gh_token_help()
+#uma vez por projeto
+use_github()
